cd0f1659f69917e664b15323f61d5ee43c172f73
Gurjit Dhaliwal <55417421+dhaliwalg@users.noreply.github.com>
2022-08-07 19:24:18 -0400

    fix

---
+++ portfolio/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+import Landing from './pages/Landing';
+import { NaviBarComponent } from './components/NavigationBar';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Classes from './pages/Classes';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import ParticlesComponent from './components/Particles';
+
+function App() {
+  return(
+    <div className="App">
+        <NaviBarComponent />
+        <ParticlesComponent className='particles' />
+            <Routes>
+                <Route exact path="/" element={<Landing />} />
+                <Route exact path="/classes" element={<Classes/>} />
+                <Route exact path="/projects" element={<Projects/>}/>
+                <Route exact path="/about" element={<About/>}/>
+            </Routes>
+        </div>
+  );
+}
+
+export default App;
