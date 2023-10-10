c999d85a812c11eb50a0c26b72ea82c07c124e6a
Gurjit Dhaliwal <55417421+dhaliwalg@users.noreply.github.com>
2023-10-10 13:12:49 -0400

    Update FooterBar.js

--- portfolio/src/components/FooterBar.js
+++ portfolio/src/components/FooterBar.js
@@ -8,10 +8,12 @@ import { FiGithub } from "react-icons/fi";
 class FooterBar extends React.Component {
     render() {
         return (
-            <IconContext.Provider value={{ color: "whitesmoke", className: "footerbar", size: "3vmax" }}>
+            <IconContext.Provider value={{ color: "whitesmoke", className: "footerbar", size: "2.5vmax" }}>
                 <div>
                     <a title="GitHub" href='https://github.com/dhaliwalg'><FiGithub className="icons" /></a>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                     <a title='Cydia Repo' href='cydia://url/https://cydia.saurik.com/api/share#?source=https://dhaliwalg.github.io/'><FiBox className="icons" /></a>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                     <a title="LinkedIn" href='https://www.linkedin.com/in/gurjit-dhaliwal/'><FiLinkedin className="icons" /></a>
                 </div>
             </IconContext.Provider>
