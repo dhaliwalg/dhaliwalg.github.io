1416ff69f7fc0762140c46a5b66cfcbc1b1322bf
Gurjit Dhaliwal <55417421+dhaliwalg@users.noreply.github.com>
2023-10-10 18:19:37 -0400

    fixing card presentation on mobile and making it classier
    sexy cards

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,77 +1,118 @@
-import React, { Component } from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { styled } from '@mui/material/styles';
 import './projects.css';
+import React, { Component } from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(.5),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+// const projects = [
+//   {
+//       'picture': 'zyx',
+//       'title' : 'title',
+//       'descr'
+//   }
+// ]
 
 class Classes extends Component {
   render() {
     return (
       <div className="App-header">
         <div className="grid">
-          <Grid container spacing={12}>
+          <MDBRow style={{ padding: '2.5vmax', maxHeight: '100vh', overflow: 'auto' }}>
+            <MDBCol md='3'>
+              <Card sx={{ maxWidth: 320 }}>
+                <CardActionArea href="https://github.com/MichaelG922/Smarternships" target="_blank">
+                  <CardMedia
+                    component="img"
+                    height="230"
+                    image="https://raw.githubusercontent.com/MichaelG922/Smarternships/main/new_logo.png"
+                    alt="Smarternships Logo"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Smarternships
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      An Android application written in Kotlin with Firebase to store encrypted logins. The platform is
+                      similar to Fiverr, allowing students and companies to connect and fill jobs.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </MDBCol>
 
-            <Grid item xs={3}>
-              <Item>
-                <Card >
-                  <Card.Img variant="top" src='https://raw.githubusercontent.com/MichaelG922/Smarternships/main/new_logo.png' />
-                  <Card.Body>
-                    <Card.Title>smarternships</Card.Title>
-                    <Card.Text>
-                      an android application written in kotlin. similar to fiverr, where students and companies can connect and fulfill jobs.
-                    </Card.Text>
-                    <Button href="https://github.com/MichaelG922/Smarternships" variant="primary">Let's see it!</Button>
-                  </Card.Body>
-                </Card>
-              </Item>
-            </Grid>
+            <MDBCol md='3'>
+              <Card sx={{ maxWidth: 320 }}>
+                <CardActionArea href="https://github.com/dhaliwalg/320finalproj" target="_blank">
+                  <CardMedia
+                    component="img"
+                    height="230"
+                    image="https://preview.redd.it/shpmpl91zk761.png?width=1000&format=png&auto=webp&s=9cfd77d2b414ae91725379294b7ab6370238e409"
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      DataSci + ML
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      An exploratory analysis in Python of all VALORANT professional matches from launch until May 2021,
+                      with hypothesis testing & machine learning.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </MDBCol>
 
-            <Grid item xs={3}>
-              <Item>
-                <Card>
-                  <Card.Img variant="top" src="https://preview.redd.it/shpmpl91zk761.png?width=1000&format=png&auto=webp&s=9cfd77d2b414ae91725379294b7ab6370238e409" />
-                  <Card.Body>
-                    <Card.Title>data science final</Card.Title>
-                    <Card.Text>
-                      an exploratory analysis in python of all valorant professional matches from launch until may 2021 with machine learning.
-                    </Card.Text>
-                    <Button href="https://dhaliwalg.github.io/320finalproj/" variant="primary">Let's see it!</Button>
-                  </Card.Body>
-                </Card>
-              </Item>
-            </Grid>
+            <MDBCol md='3'>
+              <Card sx={{ maxWidth: 320 }}>
+                <CardActionArea href="https://github.com/dhaliwalg/tweaks" target="_blank">
+                  <CardMedia
+                    component="img"
+                    height="230"
+                    image="https://raw.githubusercontent.com/dhaliwalg/dhaliwalg.github.io/master/CydiaIcon.png"
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      iOS Exploits
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      A personal passion project of reverse-engineering and injecting 3rd party code on
+                      jailbroken iOS softwares, developed in Obj-C using THEOS.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </MDBCol>
 
-            <Grid item xs={3}>
-              <Item>
-                <Card className='jb_card'>
-                  <Card.Img variant="top" src="https://raw.githubusercontent.com/dhaliwalg/dhaliwalg.github.io/master/CydiaIcon.png" />
-                  <Card.Body>
-                    <Card.Title>ios jailbreak development</Card.Title>
-                    <Card.Text>
-                      a personal passion for reverse-engineering and injecting 3rd party code in obj-c to add functionality to jailbroken iOS firmwares.
-                    </Card.Text>
-                    <Button href="https://github.com/dhaliwalg/tweaks" variant="primary">Let's see it!</Button>
-                  </Card.Body>
-                </Card>
-              </Item>
-            </Grid>
-
-          </Grid>
-
+            <MDBCol md='3'>
+              <Card sx={{ maxWidth: 320 }}>
+                <CardActionArea href="https://github.com/dhaliwalg/tweaks" target="_blank">
+                  <CardMedia
+                    component="img"
+                    height="230"
+                    image="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png"
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      React Portfolio
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      A self-initiated project of building this web application using React, JavaScript, TypeScript,
+                      and other web utilities. Constantly updating as new features are learned.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </MDBCol>
+          </MDBRow>
         </div>
+
         <p className="text">more projects on the way 😀</p>
-      </div>
+      </div >
     )
   }
 }
